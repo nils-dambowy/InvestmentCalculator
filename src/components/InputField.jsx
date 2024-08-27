ae328bf00c193e96611d9bd025aec06d0a1126b7
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-08-27 19:32:56 +0200

    updated input field component

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -2,14 +2,18 @@ export default function InputField(children) {
     return (
         <div id="user-input">
             <div id="input-group">
-                <label >Initial Investment</label>
-                <input type="number"/>
-                <label >Annual Investment</label>
-                <input type="number" />
-                <label >Expected return</label>
-                <input type="number"/>
-                <label >Duration</label>
-                <input type="number" />
+                <div>
+                    <label >Initial Investment</label>
+                    <input type="number"/>
+                    <label >Annual Investment</label>
+                    <input type="number" />
+                </div>
+                <div>
+                    <label >Expected return</label>
+                    <input type="number"/>
+                    <label >Duration</label>
+                    <input type="number" />
+                </div>
             </div>
         </div>
 
