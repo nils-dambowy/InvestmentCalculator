a30fd748f2f376ce2fcf195fad6d56a7e3fe547f
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-08-28 21:03:33 +0200

    fixed input field(finally :) )

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,23 +1,27 @@
 export default function InputField(children) {
     return (
-        <div id="user-input">
-            <div id="input-group">
-                <div>
-                    <label >Initial Investment</label>
-                    <input type="number"/>
-                    <label >Annual Investment</label>
-                    <input type="number" />
-                </div>
-                <div>
-                    <label >Expected return</label>
-                    <input type="number"/>
-                    <label >Duration</label>
-                    <input type="number" />
-                </div>
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                <label >Expected return</label>
+                <input type="number"/>
+                </p>
+                <p>
+                <label >Expected return</label>
+                <input type="number"/>
+                </p>
             </div>
-        </div>
-
-        
-
+            <div className="input-group">
+                <p>
+                <label >Expected return</label>
+                <input type="number"/>
+                </p>
+                <p>
+                <label >Expected return</label>
+                <input type="number"/>
+                </p>
+            </div>
+           
+        </section>
     )
 }
